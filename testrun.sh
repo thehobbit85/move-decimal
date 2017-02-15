ec30a1ee081c6c953d80b24434c0b65d144aa125
thehobbit85 <thehobbit85@gmail.com>
2017-02-15 03:54:50 +0200

    added msg to benchmark logs

--- testrun.sh
+++ testrun.sh
@@ -4,6 +4,10 @@ touch testResult.txt
 now=`date +%Y-%m-%d.%H:%M:%S`
 msg="-----Testing time: $now-----"
 echo $msg >> testResult.txt
+msg="-----Number of iterations is: $ITERATIONS-----"
+echo $msg >> testResult.txt
+msg="-----Number of runs is: $RUNS-----"
+echo $msg >> testResult.txt
 for i in `seq 1 $RUNS`;
 do
   msg="--------------Test number $i---------------"
