1167aaee4ee930e29a80849d2d32d21b04c5786e
thehobbit85 <thehobbit85@gmail.com>
2017-03-02 22:49:22 +0200

    added negative number tests and split fixtures to normal test and benchmark

--- test/test.js
+++ test/test.js
@@ -4,29 +4,32 @@ const jf = require('jsonfile')
 const path = require('path')
 const expect = require('chai').expect
 const ITERATIONS = parseInt(process.env.ITERATIONS, 10) || 1
-const fixtures = jf.readFileSync(path.join(__dirname, './fixtures.json'))
+const mode = process.env.npm_lifecycle_event
+const fixturesFileName = mode === 'benchmark' ? 'fixturesBenchmark.json' : 'fixtures.json'
+const fixtures = jf.readFileSync(path.join(__dirname, fixturesFileName))
+const benchmark = (mode === 'benchmark')
 
 let test = function (params, func, msg) {
-  console.time(msg)
+  benchmark && console.time(msg)
   Array(ITERATIONS).fill(0).forEach(function (a, i) {
     params.forEach(function (param, index) {
       expect(func(param[0], param[1])).to.equal(param[2])
     })
   })
-  console.timeEnd(msg)
+  benchmark && console.timeEnd(msg)
 }
 
 describe('Move decimal', function () {
   this.timeout(0)
 
   it('should return correct values for positive n', function (done) {
-    test(fixtures.positive, moveDecimalPoint, 'Testing time for move-decimal-point with positive n')
+    benchmark && test(fixtures.positive, moveDecimalPoint, 'Testing time for move-decimal-point with positive n')
     test(fixtures.positive, moveDecimal, 'Testing time for this version with positive n')
     done()
   })
 
   it('should return correct values for negative n', function (done) {
-    test(fixtures.negative, moveDecimalPoint, 'Testing time for move-decimal-point with negative n')
+    benchmark && test(fixtures.negative, moveDecimalPoint, 'Testing time for move-decimal-point with negative n')
     test(fixtures.negative, moveDecimal, 'Testing time for this version with negative n')
     done()
   })
