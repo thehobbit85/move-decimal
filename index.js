11df7c866f600e4d024367510c827bac450917d9
thehobbit85 <thehobbit85@gmail.com>
2017-03-02 22:48:38 +0200

    now works with negative number strings

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-module.exports = (num, n) => {
-  let i, r, l
-  for (i = 0; i < num.length; i++) if (num[i] === '.') break
-  if (i < num.length) {
-    l = num.slice(0, i) || '0'
-    r = num.slice(i + 1, num.length) || '0'
+module.exports = (n, p) => {
+  let i, r, l, s, len
+  s = n[0] === '-' ? '-' : ''
+  n = s ? n.replace('-', '') : n
+  len = n.length
+  for (i = 0; i < len; i++) if (n[i] === '.') break
+  if (i < len) {
+    l = n.slice(0, i) || '0'
+    r = n.slice(i + 1, len) || '0'
   } else {
-    l = num || '0'
+    l = n || '0'
     r = '0'
   }
   i = 0
-  if (n > 0) {
-    let { zeroes, move } = { zeroes: n - r.length, move: n - r.length > 0 ? r.length : n }
-    l += r.slice(0, zeroes > 0 ? r.length : n)
-    while (l[i] === '0' && i < l.length - 1) i++
-    l = l.substr(i)
-    if (zeroes < 0) return l + '.' + r.slice(move, r.length)
-    else if (zeroes > 0) for (i = 0; i < zeroes; i++) l = l + '0'
+  if (p > 0) {
+    len = r.length
+    let { z, m } = { z: p - len, m: p - len > 0 ? len : p }
+    l += r.slice(0, z > 0 ? len : p)
+    let llen = l.length - 1
+    while (l[i] === '0' && i < llen) i++
+    l = s + l.substr(i)
+    if (z < 0) return l + '.' + r.slice(m, len)
+    else if (z > 0) for (i = 0; i < z; i++) l = l + '0'
     return l
-  } else if (n < 0) {
-    n *= -1
-    let { zeroes, move } = { zeroes: n - l.length, move: n - l.length > 0 ? l.length : n }
-    r = l.slice(l.length - move, l.length) + r
-    l = l.slice(0, l.length - move)
-    while (r[r.length - (i + 1)] === '0') i++
+  } else if (p < 0) {
+    p *= -1
+    len = l.length
+    let { z, m } = { z: p - len, m: p - len > 0 ? len : p }
+    r = l.slice(len - m, len) + r
+    l = s + l.slice(0, len - m)
+    let rlen = r.length
+    while (r[rlen - (i + 1)] === '0') i++
     if (i) r = r.slice(0, -1 * i)
-    if (zeroes < 0) return r.length === 0 ? l : l + '.' + r
-    else if (zeroes > 0) for (i = 0; i < zeroes; i++) r = '0' + r
-    return '0.' + r
+    if (z < 0) return r.length === 0 ? l : l + '.' + r
+    else if (z > 0) for (i = 0; i < z; i++) r = '0' + r
+    return s + '0.' + r
   }
-  return num
+  return s + n
 }
